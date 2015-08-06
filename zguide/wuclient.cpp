41911416b9dcb6905fa672f7ed940c496798dc68
Jason Shipman <jasonpshipman@gmail.com>
2015-08-05 21:55:29 -0400

    zguide's wuserver and wuclient.

---
+++ zguide/wuclient.cpp
@@ -0,0 +1,46 @@
+// Weather update client in C++, using CpperoMQ
+// Connects SUB socket to tcp://localhost:5556
+// Collects weather updates and finds avg temp in zipcode
+
+#include <CpperoMQ/All.hpp>
+
+#include <iostream>
+#include <sstream>
+
+int main(int argc, char *argv[])
+{
+    using namespace CpperoMQ;
+
+    std::cout << "Collecting updates from weather server..." << std::endl;
+
+    // Prepare context and socket to talk to server
+    Context context;
+    SubscribeSocket subscriber(context.createSubscribeSocket());
+    subscriber.connect("tcp://localhost:5556");
+
+    // Subscribe to zip code, default is NYC, 10001
+    const char* filter = (argc > 1) ? argv[1] : "10001 ";
+    subscriber.subscribe(filter);
+
+    // Process 100 updates
+    int updateNumber = 0;
+    long totalTemp = 0;
+
+    for (updateNumber = 0; updateNumber < 100; updateNumber++)
+    {
+        IncomingMessage update;
+        subscriber.receive(update);
+
+        int zipCode = 0, temperature = 0, relativeHumidity = 0;
+        std::istringstream iss(static_cast<const char*>(update.data()));
+        iss >> zipCode >> temperature >> relativeHumidity ;
+
+        totalTemp += temperature;
+    }
+
+    std::cout << "Average temperature for zipCode '"<< filter
+              << "' was "<<(int) (totalTemp / updateNumber) <<"F"
+              << std::endl;
+
+    return 0;
+}
---
+++ zguide/wuserver.cpp
@@ -0,0 +1,44 @@
+// Weather update server in C++, using CpperoMQ
+// Binds PUB socket to tcp://*:5556
+// Publishes random weather updates
+
+#include <CpperoMQ/All.hpp>
+
+#include <random>
+#include <sstream>
+
+int main()
+{
+    using namespace CpperoMQ;
+
+    // Prepare our context and publisher
+    Context context;
+    PublishSocket publisher(context.createPublishSocket());
+    publisher.bind("tcp://*:5556");
+
+    // Initialize random number generator
+    std::random_device randomDevice;
+    std::mt19937 mersenneTwister(randomDevice());
+
+    // Initialize uniform distributions.
+    std::uniform_int_distribution<int> distZipCode(1, 99999);
+    std::uniform_int_distribution<int> distTemp(1, 215);
+    std::uniform_int_distribution<int> distHumidity(1, 50);
+
+    while (true)
+    {
+        // Get values that will fool the boss
+        int zipCode     = distZipCode(mersenneTwister);
+        int temperature = distTemp(mersenneTwister) - 80;
+        int relativeHumidity = distHumidity(mersenneTwister) + 10;
+
+        std::stringstream ss;
+        ss << zipCode << " " << temperature << " " << relativeHumidity;
+
+        // Send message to all subscribers
+        OutgoingMessage message(ss.str().length(), ss.str().c_str());
+        publisher.send(message);
+    }
+
+    return 0;
+}
