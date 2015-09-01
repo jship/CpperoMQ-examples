deaa667201206c55627fec01066b0f527396def4
Jason Shipman <jasonpshipman@gmail.com>
2015-09-01 00:34:36 -0400

    Update pubsubsubscriber to receive messages in the more user-friendly way.

--- extra/pubsubsubscriber.cpp
+++ extra/pubsubsubscriber.cpp
@@ -14,28 +14,19 @@ int main()
     SubscribeSocket subscriber(context.createSubscribeSocket());
     subscriber.subscribe(); // Subscribe to all messages.
     subscriber.connect("tcp://localhost:5560");
-    
+
     for (int i = 0; i < 10; ++i)
     {
         std::cout << "Received message " << i << ": ";
 
-        std::string curFrame;
-        bool more = true;
+        IncomingMessage inMsg1, inMsg2, inMsg3, inMsg4, inMsg5;
+        subscriber.receive(inMsg1, inMsg2, inMsg3, inMsg4, inMsg5);
 
-        while (more)
-        {
-            IncomingMessage inMsg;
-            inMsg.receive(subscriber, more);
-        
-            std::cout << std::string(inMsg.charData(), inMsg.size());
-
-            if (more)
-            {
-                std::cout << "|";
-            }
-        }
-
-        std::cout << std::endl;
+        std::cout << std::string(inMsg1.charData(), inMsg1.size()) << "|"
+                  << std::string(inMsg2.charData(), inMsg2.size()) << "|"
+                  << std::string(inMsg3.charData(), inMsg3.size()) << "|"
+                  << std::string(inMsg4.charData(), inMsg4.size()) << "|"
+                  << std::string(inMsg5.charData(), inMsg5.size()) << std::endl;
     }
 
     return 0;
